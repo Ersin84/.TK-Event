cef5020bdb578cc65b287d4b839694a91269566f
wromo <info.wromo@gmail.com>
2022-07-09 22:13:48 +0200

    add09-07-1

--- next.config.js
+++ next.config.js
@@ -15,8 +15,7 @@
  */
 
 // google adsense
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7005479426047475"
-     crossorigin="anonymous"></script> // end google adsense
+
 
 module.exports = {
   images: {
